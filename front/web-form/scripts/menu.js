de895c6aacff6bd2a4d4b200b67bca27d4dd6522
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-04-03 09:19:47 +0200

    initial client-db-connection

--- front/web-form/scripts/menu.js
+++ front/web-form/scripts/menu.js
@@ -1,3 +1,36 @@
 "use strict";
-const optionBtn = document.getElementById("option-btn");
+const contactForm = document.getElementById("contact-form");
 const contactBtn = document.getElementById("contact-btn");
+const token = localStorage.getItem("token");
+let switches = Array.from(document.getElementsByClassName("form-check-input"));
+function init() {
+}
+switches.forEach((opt) => {
+    const optionName = opt.id;
+    const isChecked = opt.checked;
+    opt.addEventListener("change", () => {
+        fetch("https://floating-escarpment-35869.herokuapp.com/update_user", {
+            method: 'POST',
+            body: JSON.stringify({ optionName: isChecked }),
+            headers: {
+                'Content-Type': 'application/json',
+                "x-access-token": token ? token : "0"
+            }
+        }).catch(err => console.error("Error:", err));
+    });
+});
+contactBtn === null || contactBtn === void 0 ? void 0 : contactBtn.addEventListener("click", () => {
+    const name = document.getElementById("contact-name-input");
+    const number = document.getElementById("contact-number-input");
+    fetch("https://floating-escarpment-35869.herokuapp.com/update_user", {
+        method: "POST",
+        body: JSON.stringify({
+            "name": name,
+            "number": number
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            "x-access-token": token ? token : "0"
+        }
+    });
+});
--- front/web-form/scripts/menu.ts
+++ front/web-form/scripts/menu.ts
@@ -1,5 +1,12 @@
 const contactForm = document.getElementById("contact-form");
+const contactBtn = document.getElementById("contact-btn");
+const token = localStorage.getItem("token");
+
 let switches = Array.from(document.getElementsByClassName("form-check-input")) as HTMLInputElement[];
+
+function init(){
+    
+}
 
 switches.forEach((opt) => {
     const optionName = opt.id;
@@ -7,12 +14,29 @@ switches.forEach((opt) => {
     opt.addEventListener("change", () => {
         fetch("https://floating-escarpment-35869.herokuapp.com/update_user", {
             method: 'POST',
-            body: JSON.stringify({optionName : isChecked})
+            body: JSON.stringify({optionName : isChecked}),
+            headers: {
+                'Content-Type': 'application/json',
+                "x-access-token" : token ? token : "0"
+              }
         }).catch(err => console.error("Error:", err));
     });
 });
 
-function init(){
-    
-}
+contactBtn?.addEventListener("click", () => {
+    const name = document.getElementById("contact-name-input");
+    const number = document.getElementById("contact-number-input");
+    fetch("https://floating-escarpment-35869.herokuapp.com/update_user", {
+        method: "POST",
+        body: JSON.stringify({
+            "name" : name,
+            "number" : number
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            "x-access-token" : token ? token : "0"
+          }
+    })
+
+})
 
