5b1727fe99ce9998c05616c418089fdaf769fc38
Piotrek <piotr.kica.35@gmail.com>
2022-04-03 03:31:17 +0200

    backend maybe works

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+CONNECTION_STRING = "mongodb+srv://PDDaccount:hgW2JY5mAx6DbQuo@" \
+                    "cluster0.dfm8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+SALT = b'$2b$12$k9909hKok3flvV.iK6X81O'
+SECRET_KEY = 'GDtfDCFYjD'
