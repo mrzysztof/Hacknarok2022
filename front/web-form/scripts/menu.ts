af99645fb40fa400af3145f5960e8d8b29054aac
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-04-03 11:05:34 +0200

    sa

--- front/web-form/scripts/menu.ts
+++ front/web-form/scripts/menu.ts
@@ -4,34 +4,31 @@ const token = localStorage.getItem("token");
 
 let switches = Array.from(document.getElementsByClassName("form-check-input")) as HTMLInputElement[];
 
-function init(){
-    
-}
 
 switches.forEach((opt) => {
-    const optionName = opt.id;
-    const isChecked = opt.checked;
+    let fd = new FormData();
+    fd.append(opt.id, opt.checked.toString());
     opt.addEventListener("change", () => {
         fetch("https://floating-escarpment-35869.herokuapp.com/update_user", {
             method: 'POST',
-            body: JSON.stringify({optionName : isChecked}),
+            body: fd,
             headers: {
                 'Content-Type': 'application/json',
-                "x-access-token" : token ? token : "0"
+                "x-access-token" : token ? token : ""
               }
         }).catch(err => console.error("Error:", err));
     });
 });
 
 contactBtn?.addEventListener("click", () => {
-    const name = document.getElementById("contact-name-input");
-    const number = document.getElementById("contact-number-input");
+    let fd = new FormData();
+    const name = document.getElementById("contact-name-input") as HTMLInputElement;
+    const number = document.getElementById("contact-number-input") as HTMLInputElement;
+    fd.append("name" , name.value);
+    fd.append("number" , number.value);
     fetch("https://floating-escarpment-35869.herokuapp.com/update_user", {
         method: "POST",
-        body: JSON.stringify({
-            "name" : name,
-            "number" : number
-        }),
+        body: fd,
         headers: {
             'Content-Type': 'application/json',
             "x-access-token" : token ? token : "0"
