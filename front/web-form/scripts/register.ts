10e48b4a35a3dd694f7a1b277fe44e343173aab6
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-04-06 23:39:15 +0200

    register page

--- front/web-form/scripts/register.ts
+++ front/web-form/scripts/register.ts
@@ -1,14 +1,31 @@
+const baseURL = "https://floating-escarpment-35869.herokuapp.com";
 const registerButton = document.getElementById("register-btn");
-const registerForm = document.getElementById("register-form") as HTMLFormElement;
 
-registerButton?.addEventListener("click", (ev) => {
-    ev.preventDefault();
+registerButton?.addEventListener("click", () => {
+    const email = document.getElementById("email-input") as HTMLInputElement;
     const password = document.getElementById("password-input") as HTMLInputElement;
     const repPassword = document.getElementById("rep-password-input") as HTMLInputElement;
+    
     if(password.value !== repPassword.value){
         alert("Provided passwords do not match");
     }
     else{
-        registerForm.submit();
+        const formData = new FormData();
+        formData.append("user_name" , email.value);
+        formData.append("password" , password.value);
+        fetch(new URL("update_user", baseURL).toString(), {
+            method: "POST",
+            body: formData
+        })
+        .then(response => {
+            if(response.status == 200){
+                alert("Registered successfully. You can login now.");
+                window.location.href = new URL("login", baseURL).toString();
+            }
+            else if(response.status == 400){
+                alert("Provided email is already in use.");
+            }
+        })
+        .catch(err => console.error(err));
     }
 });
