6e784e883df19fe602df8b16a63c4e73e63285c5
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-04-06 22:03:53 +0200

    update mongo config

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-CONNECTION_STRING = "mongodb+srv://PDDaccount:hgW2JY5mAx6DbQuo@" \
-                    "cluster0.dfm8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://TFE_Developer:eNSGwSpWDH29bvAj@" \
+                    "cluster0.dfm8k.mongodb.net"
 SALT = b'$2b$12$k9909hKok3flvV.iK6X81O'
 SECRET_KEY = 'GDtfDCFYjD'
